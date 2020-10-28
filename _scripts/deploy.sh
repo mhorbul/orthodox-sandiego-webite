036b629a1ca654cfc27ef23875c6b4eaf9cbb3f4
Maksim Horbul <max@gorbul.net>
2020-10-28 00:58:51 -0700

    fix the styles caching

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+mv _config.yml _config.yml.orig
+ruby -ryaml -e "c=YAML.load_file('_config.yml.orig'); c['time']=Time.now; puts c.to_yaml" > _config.yml
 bundle exec jekyll build
+mv _config.yml.orig _config.yml
+
 rsync -av --delete ./_site/ gorbul.net:/var/www/orthodox-sandiego.net/
