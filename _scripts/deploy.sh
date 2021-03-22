51b9960bed9f8781448c9db52c97f3a1cc628d4c
Maksim Horbul <max@gorbul.net>
2021-03-21 23:45:00 -0700

    update theme

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 mv _config.yml _config.yml.orig
 ruby -ryaml -e "c=YAML.load_file('_config.yml.orig'); c['time']=Time.now; puts c.to_yaml" > _config.yml
+export JEKYLL_ENV=production
 bundle exec jekyll build
 mv _config.yml.orig _config.yml
 
